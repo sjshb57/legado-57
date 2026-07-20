40d6fcee67f6eb82c540b99c5ffe77b50ef610d3
拜託 <98471248+sjshb57@users.noreply.github.com>
2026-07-20 19:26:50 +0800

    Update Stripped_Lite.js

--- Stripped_Lite.js
+++ Stripped_Lite.js
@@ -7,16 +7,17 @@
         preserveSuffixes: '卷次'
     };
 
-    var escaped = Config.punctuationsToRemove.replace(/[-\]\\\^[\/.*+?()|{}$]/g, '\\$&');
+    var punctEscape = /[-\]\\\^[\/.*+?()|{}$]/g;
+    var escaped = Config.punctuationsToRemove.replace(punctEscape, '\\$&');
     var baseSuffixes = '章节回集卷部篇话讲段';
     var extraSuffixes = '';
     for (var si = 0; si < Config.preserveSuffixes.length; si++) {
         var sch = Config.preserveSuffixes.charAt(si);
-        var schEscaped = sch.replace(/[-\]\\\^[\/.*+?()|{}$]/g, '\\$&');
+        var schEscaped = sch.replace(punctEscape, '\\$&');
         if (baseSuffixes.indexOf(sch) < 0 && extraSuffixes.indexOf(sch) < 0) extraSuffixes += schEscaped;
     }
     var allSuffixes = baseSuffixes + extraSuffixes;
-    var timeUnitsBase = '季年月日周天期';
+    var timeUnitsBase = '季年月日周天期次';
     var timeUnits = '';
     for (var ti = 0; ti < timeUnitsBase.length; ti++) {
         var tch = timeUnitsBase.charAt(ti);
@@ -25,9 +26,9 @@
     var Regex = {
         patterns: {
             chapter: new RegExp('^第(\\d+|[零〇一二三四五六七八九十百千两万壹贰叁肆伍陆柒捌玖拾佰仟萬貳參陸]+)([' + allSuffixes + ']?)(.*)'),
-            digitalChapterWithSuffix: new RegExp('^(\\d+)([' + allSuffixes + '])(.*)'),
-            digitalChapter: /^(\d+)\s*(.*)/,
+            digitalChapter: new RegExp('^(\\d+)([' + allSuffixes + ']?)(.*)'),
             digitalTimeUnit: timeUnits ? new RegExp('^\\d+[' + timeUnits + ']') : null,
+            leadingTimeUnit: timeUnits ? new RegExp('^[' + timeUnits + ']') : null,
             chinesePunctuation: /^[：、；？！。…]/,
             leadingPunctuations: new RegExp('^[\\s' + escaped + ']+'),
             trailingPunctuations: new RegExp('[\\s' + escaped + ']+$'),
@@ -57,10 +58,12 @@
             '七': 7,
             '八': 8,
             '九': 9,
+
             '十': 10,
             '百': 100,
             '千': 1000,
             '万': 10000,
+
             '壹': 1,
             '贰': 2,
             '叁': 3,
@@ -74,6 +77,7 @@
             '佰': 100,
             '仟': 1000,
             '萬': 10000,
+
             '貳': 2,
             '參': 3,
             '陸': 6
@@ -116,6 +120,11 @@
         }
     }
 
+    function resolveSuffix(originalSuffix) {
+        return (originalSuffix && Config.preserveSuffixes.indexOf(originalSuffix) >= 0) ?
+            originalSuffix : Config.defaultSuffix;
+    }
+
     function processTitle(title) {
         var bracketBlacklist = /[票更快祝新迎章订架求免]/;
         title = title.replace(/[（(]([^）)]*)[）)]/g, function(_, inner) {
@@ -129,27 +138,18 @@
         if (/^\d+\.\d/.test(title)) {
             var stripped = title.replace(/^[\d.]+/, '').replace(/^\s+|\s+$/g, '');
 
-            return stripped ? stripped.replace(Regex.patterns.allPunctuations, '') : title;
+            if (!stripped) return title;
+            title = stripped;
         }
 
         var cleaned = title.replace(Regex.patterns.allPunctuations, '');
-
-        var suffixMatch = title.match(Regex.patterns.digitalChapterWithSuffix);
-        if (suffixMatch) {
-            var sNum = suffixMatch[1];
-            var sOrigSuffix = suffixMatch[2];
-            var sTitlePart = suffixMatch[3] || '';
-            var sSuffix = (Config.preserveSuffixes.indexOf(sOrigSuffix) >= 0) ?
-                sOrigSuffix : Config.defaultSuffix;
-            return assemble('第' + sNum, sSuffix, sTitlePart);
-        }
 
         if (Regex.patterns.digitalTimeUnit && Regex.patterns.digitalTimeUnit.test(title)) {
             return cleaned;
         }
         var digitalMatch = title.match(Regex.patterns.digitalChapter);
         if (digitalMatch) {
-            return assemble('第' + digitalMatch[1], Config.defaultSuffix, digitalMatch[2]);
+            return assemble('第' + digitalMatch[1], resolveSuffix(digitalMatch[2]), digitalMatch[3]);
         }
 
         if (title.charAt(0) !== '第') return cleaned;
@@ -161,18 +161,17 @@
         var originalSuffix = match[2];
         var titlePart = match[3] || '';
 
-        if (!originalSuffix && /^[季年月日周天期次]/.test(titlePart.replace(/^\s+/, ''))) {
+        if (!originalSuffix && Regex.patterns.leadingTimeUnit &&
+            Regex.patterns.leadingTimeUnit.test(titlePart.replace(/^\s+/, ''))) {
             return cleaned;
         }
 
-        var suffix = (originalSuffix && Config.preserveSuffixes.indexOf(originalSuffix) >= 0) ?
-            originalSuffix : Config.defaultSuffix;
+        var suffix = resolveSuffix(originalSuffix);
 
         var number = /^\d+$/.test(chineseNum) ? chineseNum : NumberConverter.convert(chineseNum);
 
         if (Number(number) === 0 && Config.convertZeroToPreface) {
-            number = '';
-            suffix = '序' + suffix;
+            return assemble('', '序' + suffix, titlePart);
         }
         return assemble('第' + number, suffix, titlePart);
     }
